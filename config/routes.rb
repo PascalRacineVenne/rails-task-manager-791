50da3cb8b44d5b7323ffe362588faf305139f0c2
Pascal Racine-Venne <pascalracinevenne@gmail.com>
2022-02-14 22:13:53 -0500

    routes in action

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # read all
+  get '/tasks', to: 'tasks#index'
+  # create one
+  get 'task/new', to: 'tasks/#new', as: :new_task
+  # read one
+  get '/tasks/:id', to: 'tasks#show', as: :task
+  post '/tasks', to: 'tasks#create'
+  # update one
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch '/tasks/:id', to: 'tasks#update'
+  # delete one
+  delete '/tasks/:id', to: 'tasks#destroy'
 end
